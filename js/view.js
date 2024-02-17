af7e515c0fa5eedaf5bffca31f8034a662b45e49
megh16123 <meghansh2002@gmail.com>
2024-02-17 23:12:00 +0530

    request handler updated

--- js/view.js
+++ js/view.js
@@ -6,18 +6,20 @@ const populate_table = (arr) =>{
         table.innerHTML+=`<tr><td>${items['fname']}</td><td>${items['size']}</td></tr>`;
     });
 }
-
+const processarr=(content)=>{
+console.log(content);
+}
 window.addEventListener("load",async(event)=>{
 const token = sessionStorage.getItem('token');
 if(token==null){
     window.location.replace('index.html');
 }else{
     const url = globurl+"/getlist";
-    const response = await requestHandler(url,"POST",token);
+    const content = await requestHandler(url,"POST",token);
     if(response.status !==200){
     window.location.replace('index.html');
     }else{
-        const arr = processarr(response.body);
+        const arr = processarr(content);
         populate_table(arr);
     }
 }    
