54d93f1134e89c741134848f84bf32027dd68e82
megh16123 <meghansh2002@gmail.com>
2024-02-17 12:11:16 +0530

    frontend added

---
+++ js/util.js
@@ -0,0 +1,23 @@
+const requestHandler = async(url,method,body)=>{
+const response = await fetch(url,{
+                                method:method,
+                                mode:"cors",
+                                body:body
+});
+return response;
+}
+const redirects = ()=>{
+    try {
+        
+            document.getElementById('logout').addEventListener("click",(event)=>{
+                console.log("hello")
+                window.location.replace("index.html")});   
+    } catch (error) {    }
+    try {
+         document.getElementById('back-to-menu').addEventListener("click",(event)=>{
+            window.location.replace("menu.html");
+            });
+    } catch (error) {}
+    
+    
+}
