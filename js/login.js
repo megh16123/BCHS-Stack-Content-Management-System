889060cb0bacbed8cba3e8ab92ecc62e59a2b142
megh16123 <meghansh2002@gmail.com>
2024-02-17 16:04:31 +0530

    js updated

--- js/login.js
+++ js/login.js
@@ -1,1 +1,14 @@
 redirects();
+document.getElementById("myform").addEventListener('submit',async(event)=>{
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById('password').value;
+    const url= globurl+"/someendpoint";
+    const response = await requestHandler(url,"POST",btoa(username)+","+btoa(password));
+    if(response.status===200){
+        // TODO : set the token in session storage 
+        window.location.replace("menu.html")
+    }else{
+        alert("Authentification Failed");
+    }
+})
--- js/view.js
+++ js/view.js
@@ -1,1 +1,24 @@
 redirects();
+
+const populate_table = (arr) =>{
+    const table = document.getElementById('items');
+    arr.map((items)=>{
+        table.innerHTML+=`<tr><td>${items['fname']}</td><td>${items['size']}</td></tr>`;
+    });
+}
+
+window.addEventListener("load",async(event)=>{
+const token = sessionStorage.getItem('token');
+if(token==null){
+    window.location.replace('index.html');
+}else{
+    const url = globurl+"/getlist";
+    const response = await requestHandler(url,"POST",token);
+    if(response.status !==200){
+    window.location.replace('index.html');
+    }else{
+        const arr = processarr(response.body);
+        populate_table(arr);
+    }
+}    
+});
