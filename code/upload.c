8fbac34e3689aea38daef8a19ab78079bde5de8c
napolean <talhak2111@gmail.com>
2024-02-26 19:18:26 +0530

    added new upload

--- code/upload.c
+++ code/upload.c
@@ -1,5 +1,9 @@
 /*
-	Upload utility
+	Upload utility    
+	input :- usersrting,session token,md5#,filename,filedatab64
+YWtyQHB1Y3NkMTIzNHA=,824d1507-82d6-40e3-8e83-b24cb5f2e56a,b87bac80d2ea9b4ec735d9a7327f6e2f,filename.c,aGVsbG90aGlzaXNhbmltcG9ydGFudG1lc3NhZ2U=
+	please dont forget the last ',' 
+	md5 hash will be of the file only.
 */
 
 
@@ -43,8 +47,9 @@ int main()
 	sqlite3 *curdb;
 	int ret_status_code = -1;
 	char * len = getenv("CONTENT_LENGTH");
+	char * u_name = getenv("U_NAME");
 	long ll=0;
-	if( len != NULL && (ll = atoi(len))>0 )
+	if( len != NULL && u_name != NULL &&  (ll = atoi(len))>0 )
 	{
 
 		int i=0;
@@ -58,7 +63,7 @@ int main()
 		char userID[100];
 		char token[37];
 		char fileName[100];
-
+		char filehash[33];
 		int ui=0;
 		while( userstring[ui] != ',' && ui<ll )
 		{
@@ -68,28 +73,68 @@ int main()
 		}
 		userID[ui]=0;
 		ui++;
-		if( i==ll && ui<ll &&  check_valid_user(userID) == 1 && udb[0]!=0 )
+		int ut;
+		for(  ut=0; userstring[ui]!=',' && ui<ll && ut<37 ;ut++ )
 		{
-			char db_name[40];
-			sprintf(db_name,"%s.db",udb);
+			token[ut]=userstring[ui];
+			ui++;
+		}
+		token[36]=0;
+		ui++;
+		int ud5;
+		for( ud5=0;userstring[ui]!=',' && ui<ll && ud5<32;ud5++ )
+		{
+			filehash[ud5]=userstring[ui];
+			ui++;
+		}
+		ui++;
+		filehash[ud5]=0;
+		int ufn;
+		for( ufn=0;userstring[ui]!=',' && ui<ll && ufn<50;ufn++ )
+		{
+			fileName[ufn]=userstring[ui];
+			ui++;
+		}
+		ui++;
+		char ff_name[200];
+		if( ud5 <= 32 && ut<=37 && ufn<=50 &&  i==ll && ui<ll &&  check_valid_user(userID) == 1 && udb[0]!=0 )
+		{
+			char db_name[200];
+			char hash_file_str[200];
+			sprintf(hash_file_str,"hash.%s.%s",u_name,fileName);
+			sprintf(db_name,"%s/%s.db",udb,udb);
+			sprintf(ff_name,"%s.%s",u_name,fileName);
+			FILE * write_file = fopen(ff_name,"w+");
+			FILE * hash_file = fopen(hash_file_str,"w+");
+			FILE * udb_file = fopen(u_name,"w+");
 			// 1 in active means not active 0 means acitve;
 			uint32_t db_status = sqlite3_open_v2(db_name,&curdb,SQLITE_OPEN_READWRITE,NULL);
-			if( db_status == SQLITE_OK )
+			if( write_file != NULL && hash_file!=NULL && udb_file!=NULL  && db_status == SQLITE_OK )
 			{
 				//set all previous sessions to inactive.
 				char del_query[150];
-				sprintf(del_query,"select sestkn from session where sestkn like '%s' and active=1 and (select UNIXEPOCH())-time<=60*15;",&userstring[ui]);
+				sprintf(del_query,"select sestkn from session where sestkn like '%s' and active=1 and (select UNIXEPOCH()) -time <= 60*60 ;" ,token);
 				sqlite3_stmt *st;
 				int sret=sqlite3_prepare_v2(curdb,del_query,-1,&st,NULL);
 				int stret = sqlite3_step(st);
-				if( sret == SQLITE_OK && stret == SQLITE_ROW && strcmp(&userstring[ui],sqlite3_column_text(st,0))==0 )
+				if( sret == SQLITE_OK && stret == SQLITE_ROW && strcmp(token,sqlite3_column_text(st,0))==0 )
 				{
 					sqlite3_finalize(st);
-					puts("Status: 200 OK\r");
-					puts("Content-Type: text/html\r");
-					puts("\r");
-					puts("valid\r");
-					ret_status_code=1;
+					char env_str[100];
+					sprintf(env_str,"U_NAME=%s",db_name);
+					int write_status = fwrite(&userstring[ui],1,ll-ui,write_file);
+					int hash_write_status = fprintf(hash_file,"%s %s",filehash,ff_name);
+					int udb_write_status = fprintf(udb_file,"%s",udb);
+					fclose(write_file);
+					fclose(hash_file);
+					fclose(udb_file);
+					if( write_status != 0 && hash_write_status != 0 && udb_write_status!=0 )
+					{
+						puts("Status: 200 OK\r");
+						puts("Content-Type: text/html\r");
+						puts("\r");
+						ret_status_code=1;
+					}
 				}
 				sqlite3_close(curdb);
 			}
@@ -98,16 +143,8 @@ int main()
 	if( ret_status_code==-1)
 	{
 		puts("Status: 503 ERROR\r");
+		puts("Content-Type: text/html\r");
+		puts("\rfailed");
 	}
+	return EXIT_SUCCESS;
 }
-/*
-   sqlite3 *mydb;
-   int sqlRet=sqlite3_open_v2("test.db",&mydb,SQLITE_OPEN_READWRITE,NULL);
-   if( sqlRet == SQLITE_OK )
-   {
-   char stmt[] = "insert into sample values('i',1);";
-   printf("connection established succesfully\n");
-   sqlite3_close(mydb);
-   }
-   printf("sqlRet:%d\n",sqlRet);
- */
