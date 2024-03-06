1bcf1207c4e8508110a0d07e3e927f865410915b
megh16123 <meghansh2002@gmail.com>
2024-03-06 13:36:07 +0530

    delete and other js p to date

--- js/delete.js
+++ js/delete.js
@@ -7,10 +7,11 @@ document.getElementById("myform").addEventListener('submit',async(event)=>{
     event.preventDefault();
     const fileId = document.getElementById("fileid").value;
     const url= globurl+"/rem";
-    const {content,status} = await requestHandler(url,"POST",sessionStorage.getItem('token')+","+btoa(fileId));
+    const {content,status} = await requestHandler(url,"POST",sessionStorage.getItem('token')+","+fileId);
+	console.log(content);
     if(status===200){
-    alert("File Deleted Successfully")
-    fetch_files("just text");
+    alert("File Deleted Successfully");
+	window.location.replace("delete.html");
     }else{
         console.log(content);
     }
--- js/upload.js
+++ js/upload.js
@@ -1,16 +1,16 @@
 check_session();
 redirects();
 const file_size_validator = (file) =>{
-if(file.size/1000000>4){
-	return true;
-}else{
-	return false;
-}
+	if(file.size/1000000>4){
+		return true;
+	}else{
+		return false;
+	}
 }
 
 const file_extension_validato =(file_name)=>{
 	const file =  file_name.split(".");
-	const ban = ["doc","docm","docx","docx","dot","dotm","dotx","mht","mhtml","zip","rtf","wps","xps","pptx"]
+	const ban = ["doc","docm","docx","docx","dot","dotm","dotx","mht","mhtml","zip","rtf","wps","xps","pptx","db","net","vb","asp","jsp","cs","py"]
 	console.log(file)
 	if(file.length>2){
 		alert("File Name not Correct");
@@ -29,53 +29,55 @@ const file_extension_validato =(file_name)=>{
 	}
 }
 
-const validate_file_name = (file_name) =>{
-	const url = globurl + "/chkupld";
-	const payload = sessionStorage.getItem("token") + btoa(file_name);
-	const {content,status} = requestHandler(url,"POST",payload);
-	if(status === 200){
-		return true;
+const validate_file_name = async(file_name) =>{
+	const url = globurl + "/file";
+	const payload = sessionStorage.getItem("token")+","+file_name;
+	const {content,status}= await requestHandler(url,"POST",payload);
+	if(status ===200){
+	return true
 	}else{
 		return false;
 	}
-	 
+
 }
 
 const file = document.getElementById("myinput");
 const act = document.getElementById("myform");
 
-const  handleSubmit = (event) =>{
+const  handleSubmit = async(event) =>{
 	event.preventDefault();
 	if(file_size_validator(file.files[0])){
-			alert("File too Large");
+		alert("File too Large");
 	}else{
 		file_extension_validato(file.files[0].name)
-		if(validate_file_name(file.files[0].name)){
-		reader = new FileReader();
-		reader.readAsDataURL( file.files[0] );
-		reader.onloadend = async( event ) =>{
-		if( event.target.readyState !== FileReader.DONE ){
-				return;
-		}
-		const url = globurl+"/upload";
-		const method = "POST";
-		const payload = sessionStorage.getItem("token")+","+hex_md5(event.target.result.split(",")[1])+","+file.files[0].name+","+event.target.result.split(",")[1];
-		console.log(payload);
-		const {content,status} = await requestHandler(url,method,payload);
-		console.log(content);
-        if(status===200){
-			console.log(content);
-			alert("file uploaded successfully");
-			//window.location.replace("menu.html");
-		}else{
-			alert(content)
-			//window.location.replace("menu.html");
+		const apt = await validate_file_name(file.files[0].name);
+		console.log(apt,"handle");
+		if(apt){
+			reader = new FileReader();
+			reader.readAsDataURL( file.files[0] );
+			reader.onloadend = async( event ) =>{
+				if( event.target.readyState !== FileReader.DONE ){
+					return;
+				}
+				const url = globurl+"/upload";
+				const method = "POST";
+				const payload = sessionStorage.getItem("token")+","+hex_md5(event.target.result.split(",")[1])+","+file.files[0].name+","+event.target.result.split(",")[1];
+				console.log(payload);
+				const {content,status} = await requestHandler(url,method,payload);
+				console.log(content);
+				if(status===200){
+					console.log(content);
+					alert("file uploaded successfully");
+					window.location.replace("menu.html");
+				}else{
+					alert(content)
+					window.location.replace("menu.html");
+				}
 			}
+		}else{
+			alert("File already Exists!")
 		}
-	}else{
-		alert("Something went wrong!")
 	}
-}
 }
 
 act.addEventListener('submit',handleSubmit);
